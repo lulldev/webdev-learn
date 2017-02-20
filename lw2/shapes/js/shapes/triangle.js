3785f5122ce9ec1a21663ecc8e6d6bbfb0530700
lulldev <lulldev@yandex.ru>
2017-02-20 21:53:00 +0300

    реализация Triangle и базовых тестов

--- lw2/shapes/js/shapes/triangle.js
+++ lw2/shapes/js/shapes/triangle.js
@@ -1,3 +1,34 @@
-/**
- * Created by lull on 15.02.17.
- */
+function CTriangle(fillColor = '#fff', borderColor = '#000', x1 = 10, y1 = 20, x2 = 40, y2 = 20, x3 = 50, y3 = 60) {
+    CShape.apply(this, arguments);
+    this.x1 = x1;
+    this.y1 = y1;
+    this.x2 = x2;
+    this.y2 = y2;
+    this.x3 = x3;
+    this.y3 = y3;
+}
+
+CTriangle.prototype = Object.create(CShape.prototype);
+CTriangle.prototype.constructor = CTriangle;
+
+CTriangle.prototype.calculateArea = function() {
+    var a  = Math.sqrt(Math.sqrt(Math.abs(this.x1 - this.x2)) + Math.sqrt(Math.abs(this.y1 - this.y2))),
+        b  = Math.sqrt(Math.sqrt(Math.abs(this.x2 - this.x3)) + Math.sqrt(Math.abs(this.y2 - this.y3))),
+        c  = Math.sqrt(Math.sqrt(Math.abs(this.x3 - this.x1)) + Math.sqrt(Math.abs(this.y3 - this.y1))),
+        perimeter = this.calculatePerimeter();
+
+    return parseFloat((Math.sqrt(perimeter * (perimeter - a) * (perimeter - b) * (perimeter - c))).toFixed(2));
+};
+
+CTriangle.prototype.calculatePerimeter = function() {
+    var a  = Math.sqrt(Math.sqrt(Math.abs(this.x1 - this.x2)) + Math.sqrt(Math.abs(this.y1 - this.y2))),
+        b  = Math.sqrt(Math.sqrt(Math.abs(this.x2 - this.x3)) + Math.sqrt(Math.abs(this.y2 - this.y3))),
+        c  = Math.sqrt(Math.sqrt(Math.abs(this.x3 - this.x1)) + Math.sqrt(Math.abs(this.y3 - this.y1)));
+
+    return parseFloat((2 * (a + b + c)).toFixed(2));
+};
+
+CTriangle.prototype.draw = function() {
+
+};
+
--- lw2/shapes/tests/shapes-tests.js
+++ lw2/shapes/tests/shapes-tests.js
@@ -89,4 +89,48 @@ describe("CRectangle testing", function() {
 
 });
 
+describe("CTriangle testing", function() {
 
+    describe("Init triangle", function() {
+
+        it("Init with default values", function() {
+
+            var triangle = new CTriangle();
+            assert.equal(triangle.x1, 10);
+            assert.equal(triangle.y2, 20);
+            assert.equal(triangle.x2, 40);
+            assert.equal(triangle.y2, 20);
+            assert.equal(triangle.fillColor, '#fff');
+            assert.equal(triangle.borderColor, '#000');
+        });
+
+        it("Init with optional values", function() {
+
+            var triangle = new CTriangle('#ccc', '#000', 100, 200, 300, 200);
+
+            assert.equal(triangle.x1, 100);
+            assert.equal(triangle.y2, 200);
+            assert.equal(triangle.x2, 300);
+            assert.equal(triangle.y2, 200);
+            assert.equal(triangle.fillColor, '#ccc');
+            assert.equal(triangle.borderColor, '#000');
+        });
+
+    });
+
+    describe("Calculations", function() {
+
+        var triangle = new CTriangle('#ccc', '#000', 10, 20, 50, 30, 50, 60);
+
+        it("Calculate perimeter", function() {
+            assert.typeOf(triangle.calculatePerimeter(), 'number');
+            assert.equal(triangle.calculatePerimeter(), 17.95);
+        });
+
+        it("Calculate area", function() {
+            assert.typeOf(triangle.calculateArea(), 'number');
+            assert.equal(triangle.calculateArea(), 244.89)
+        });
+    });
+
+});
